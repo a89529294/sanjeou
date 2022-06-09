324e7d399e86fdc13c87d2845234aadcb2b8fe34
Albert Chang <a89529294@hotmail.com>
2022-06-09 15:53:36 +0800

    make products page items into links

--- components/layouts/ProductPageLayout.tsx
+++ components/layouts/ProductPageLayout.tsx
@@ -3,69 +3,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import ItemTitle from "../ItemTitle";
 import HeroImage from "../HeroImage";
-
-const products = [
-  {
-    series: "防火防煙捲門系列",
-    items: [
-      { id: 1, name: "防火捲門" },
-      { id: 2, name: "阻熱捲門" },
-      { id: 3, name: "阻熱遮煙捲門" },
-      { id: 4, name: "遮煙捲幕" },
-    ],
-    icon: "/img/icons/door1.svg",
-  },
-  {
-    series: "防水防洪門系列",
-    items: [
-      { id: 5, name: "防水捲門" },
-      { id: 6, name: "新式手動插板水閘門" },
-      { id: 7, name: "電動油壓水閘門" },
-      { id: 8, name: "扇形水閘門" },
-      { id: 9, name: "手動扇形水密門" },
-      { id: 10, name: "電動扇形水密門" },
-      { id: 11, name: "電動橫移水密門" },
-    ],
-    icon: "/img/icons/door2.svg",
-  },
-  {
-    series: "抗風防颱捲門系列",
-    items: [
-      { id: 12, name: "防颱捲門" },
-      { id: 13, name: "重型防颱捲門" },
-    ],
-    icon: "/img/icons/door3.svg",
-  },
-  {
-    series: "廠辦管制門",
-    items: [
-      { id: 14, name: "密閉式捲門" },
-      { id: 15, name: "花格式捲門" },
-      { id: 16, name: "側開式PVC捲門" },
-      { id: 17, name: "uniflow" },
-    ],
-
-    icon: "/img/icons/door4.svg",
-  },
-  {
-    series: "圍牆大門",
-    items: [
-      { id: 18, name: "單段式大門" },
-      { id: 19, name: "多段式大門" },
-      { id: 20, name: "伸縮大門" },
-    ],
-    icon: "/img/icons/door5.svg",
-  },
-  {
-    series: "機庫門",
-    items: [
-      { id: 21, name: "柔性門" },
-      { id: 22, name: "橫移式機庫門" },
-    ],
-    icon: "/img/icons/door6.svg",
-  },
-  { series: "客製化", items: [], icon: "/img/icons/door7.svg" },
-];
+import products from "../../data/products";
 
 const ListItem = ({
   item,
--- components/products/Main.tsx
+++ components/products/Main.tsx
@@ -1,50 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import ItemTitle from "../ItemTitle";
 import SectionTitle from "../SectionTitle";
-
-const products = [
-  {
-    series: "防火防煙捲門系列",
-    items: ["防火捲門", "阻熱捲門", "阻熱遮煙捲門", "遮煙捲幕"],
-    icon: "/img/icons/door1.svg",
-  },
-  {
-    series: "防水防洪門系列",
-    items: [
-      "防水捲門",
-      "新式手動插板水閘門",
-      "電動油壓水閘門",
-      "扇形水閘門",
-      "手動扇形水密門",
-      "電動扇形水密門",
-      "電動橫移水密門",
-    ],
-    icon: "/img/icons/door2.svg",
-  },
-  {
-    series: "抗風防颱捲門系列",
-    items: ["防颱捲門", "重型防颱捲門"],
-    icon: "/img/icons/door3.svg",
-  },
-  {
-    series: "廠辦管制門",
-    items: ["密閉式捲門", "花格式捲門", "側開式PVC捲門", "uniflow"],
-
-    icon: "/img/icons/door4.svg",
-  },
-  {
-    series: "圍牆大門",
-    items: ["單段式大門", "多段式大門", "伸縮大門"],
-    icon: "/img/icons/door5.svg",
-  },
-  {
-    series: "機庫門",
-    items: ["柔性門", "橫移式機庫門"],
-    icon: "/img/icons/door6.svg",
-  },
-  { series: "客製化", items: [], icon: "/img/icons/door7.svg" },
-];
+import products from "../../data/products";
 
 const ProductGrid = ({
   title,
@@ -52,7 +11,7 @@ const ProductGrid = ({
   icon,
 }: {
   title: string;
-  items: string[];
+  items: typeof products[number]["items"];
   icon: string;
 }) => (
   <div>
@@ -60,14 +19,16 @@ const ProductGrid = ({
     <div className="grid mt-6 gap-x-5 gap-y-7 grid-cols-product-grid">
       {items.map((item, i) => (
         <div key={i}>
-          <div className="relative aspect-[4/3]">
-            <Image
-              layout="fill"
-              objectFit="cover"
-              src="https://placehold.jp/300x230.png"
-            />
-          </div>
-          <ItemTitle>{item}</ItemTitle>
+          <Link href={`/products/${item.id}`}>
+            <a className="relative aspect-[4/3] block">
+              <Image
+                layout="fill"
+                objectFit="cover"
+                src="https://placehold.jp/300x230.png"
+              />
+            </a>
+          </Link>
+          <ItemTitle>{item.name}</ItemTitle>
         </div>
       ))}
     </div>
